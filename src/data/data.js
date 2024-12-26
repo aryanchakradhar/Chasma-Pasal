a8be58ac9b674128aea082a9c7b51f8ea164d9a8
Aryan Chakradhar <aryanchakradhar11@gmail.com>
2024-12-26 23:18:07 +0545

    Add dummy data

---
+++ src/data/data.js
@@ -0,0 +1,118 @@
+export const ProductsData = [
+    {
+      id: 1,
+      name: 'JHJASVCJ',
+      price: '12.00',
+      brand: 'AJDAD',
+      review: 5,
+      descriptions: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: 'https://dummyimage.com/600x400/000/fff',
+      colors: [
+        {
+          name: 'Red',
+          image: 'https://dummyimage.com/600x400/000/fff'
+        },
+        {
+          name: 'Blue',
+          image: 'https://dummyimage.com/400x400/000/fff'
+        },
+        {
+          name: 'Green',
+          image: 'https://dummyimage.com/400x400/000/fff'
+        }
+      ]
+    },
+    {
+      id: 2,
+      name: 'AHSJVDVA',
+      price: '85.00',
+      brand: 'ASKB',
+      review: 4,
+      descriptions: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      image: 'https://dummyimage.com/600x400/000/fff',
+      colors: [
+        {
+          name: 'Red',
+          image: 'https://dummyimage.com/600x400/000/fff'
+        },
+        {
+          name: 'Blue',
+          image: 'https://dummyimage.com/400x400/000/fff'
+        },
+        {
+          name: 'Green',
+          image: 'https://dummyimage.com/400x400/000/fff'
+        }
+      ]
+    },
+    {
+      id: 3,
+      name: 'ABCGS',
+      price: '600.00',
+      brand: 'ABCU',
+      review: 4,
+      descriptions: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      image: 'https://dummyimage.com/600x400/000/fff',
+      colors: [
+        {
+          name: 'ABC',
+          image: 'https://dummyimage.com/600x400/000/fff'
+        },
+        {
+          name: 'DEF',
+          image: 'https://dummyimage.com/400x400/000/fff'
+        },
+        {
+          name: 'GHI',
+          image: 'https://dummyimage.com/400x400/000/fff'
+        }
+      ]
+    },
+    {
+      id: 4,
+      name: 'ABCH',
+      price: '212.00',
+      brand: 'ABC',
+      review: 4,
+      descriptions: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      image: 'https://dummyimage.com/600x400/000/fff',
+      colors: [
+        {
+          name: 'ABC',
+          image: 'https://dummyimage.com/600x400/000/fff'
+        },
+        {
+          name: 'DEF',
+          image: 'https://dummyimage.com/400x400/000/fff'
+        },
+        {
+          name: 'GHI',
+          image: 'https://dummyimage.com/400x400/000/fff'
+        }
+      ]
+    },
+    {
+      id: 5,
+      name: 'ABCDS',
+      price: '100.00',
+      brand: 'ABCD',
+      review: 3,
+      descriptions: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
+      image: 'https://dummyimage.com/600x400/000/fff',
+      colors: [
+        {
+          name: 'ABC',
+          image: 'https://dummyimage.com/600x400/000/fff'
+        },
+        {
+          name: 'DEF',
+          image: 'https://dummyimage.com/400x400/000/fff'
+        },
+        {
+          name: 'GHI',
+          image: 'https://dummyimage.com/400x400/000/fff'
+        }
+      ]
+    }
+  ]
+  
